e8f61b7e4ed1021c15a86f24b64b8e838024abdd
javadyakuza <javadyakuzaa@gmail.com>
2024-01-19 19:36:50 +0330

    general_setters_implemented

--- src/db_models.rs
+++ src/db_models.rs
@@ -6,6 +6,7 @@ use diesel::dsl::sql;
 use diesel::pg::Pg;
 use serde::{Deserialize, Serialize};
 
+// general models
 #[derive(Queryable, Selectable, Debug, Insertable)]
 #[diesel(table_name = crate::schema::wallets)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
@@ -56,6 +57,58 @@ pub struct TestCases {
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Users {
     pub user_id: i32,
+    pub email: String,
+    pub username: String,
+    pub password: String,
+    pub total_payed: i32,
+    pub total_claimed: i32,
+}
+
+// inserting models
+// the following models are not containing the primary key fields, making able the fns to insert the values without the pks.
+
+#[derive(Queryable, Selectable, Debug, Insertable)]
+#[diesel(table_name = crate::schema::responses)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct IResponses {
+    // pub response_id: i32, // pk
+    pub daredevil_id: i32,
+    pub question_id: i32,
+    pub response_code: String,
+    pub correctness: bool,
+    pub creation_time: NaiveDateTime,
+}
+
+#[derive(Queryable, Selectable, Debug, Insertable)]
+#[diesel(table_name = crate::schema::questions)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct IQuestions {
+    // pub question_id: i32, // pk
+    pub rival_id: i32,
+    pub question_title: String,
+    pub question_body: String,
+    pub creation_time: NaiveDateTime,
+    pub deadline: NaiveDateTime,
+    pub question_status: i32,
+    pub daredevil: Option<i32>,
+    pub category: String,
+}
+
+#[derive(Queryable, Deserialize, Serialize, Selectable, Debug, Insertable)]
+#[diesel(table_name = crate::schema::test_cases)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct ITestCases {
+    // pub test_case_id: i32, // pk
+    pub question_id: i32,
+    pub test_inputs: String,
+    pub test_outputs: String,
+}
+
+#[derive(Queryable, Deserialize, Serialize, Selectable, Debug, Insertable)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct IUsers {
+    // pub user_id: i32, // pk
     pub email: String,
     pub username: String,
     pub password: String,
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ pub mod api_models;
 pub mod db_models;
 pub mod schema;
 
-use crate::db_models::{Questions, Responses, TestCases, Users, Wallets};
+use crate::db_models::{
+    IQuestions, IResponses, ITestCases, IUsers, Questions, Responses, TestCases, Users, Wallets,
+};
 use crate::schema::{questions, responses, test_cases, users, wallets};
 use chrono::Local;
 pub use diesel;
@@ -25,4 +27,100 @@ pub fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+// general ** setters
+pub fn add_new_user(
+    conn: &mut PgConnection,
+    _new_user: &IUsers,
+) -> Result<Users, Box<dyn std::error::Error>> {
+    // inserting the new user
+    match diesel::insert_into(users::table)
+        .values(_new_user)
+        .returning(Users::as_returning())
+        .get_result(conn)
+    {
+        Ok(nu) => Ok(nu),
+        Err(e) => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("{:?}", e),
+            )))
+        }
+    }
+}
+
+pub fn add_user_wallet(
+    conn: &mut PgConnection,
+    _new_wallet: &Wallets,
+) -> Result<Wallets, Box<dyn std::error::Error>> {
+    // inserting the new user
+    match diesel::insert_into(wallets::table)
+        .values(_new_wallet)
+        .returning(Wallets::as_returning())
+        .get_result(conn)
+    {
+        Ok(nw) => Ok(nw),
+        Err(e) => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("{:?}", e),
+            )))
+        }
+    }
+}
+
+pub fn add_new_question(
+    conn: &mut PgConnection,
+    _new_question: &IQuestions,
+    _test_cases: &ITestCases,
+) -> Result<(Questions, TestCases), Box<dyn std::error::Error>> {
+    // inserting the new user
+    match diesel::insert_into(questions::table)
+        .values(_new_question)
+        .returning(Questions::as_returning())
+        .get_result(conn)
+    {
+        Ok(nq) => {
+            match diesel::insert_into(test_cases::table)
+                .values(_test_cases)
+                .returning(TestCases::as_returning())
+                .get_result(conn)
+            {
+                Ok(nt) => Ok((nq, nt)),
+                Err(e) => {
+                    return Err(Box::new(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        format!("{:?}", e),
+                    )))
+                }
+            }
+        }
+        Err(e) => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("{:?}", e),
+            )))
+        }
+    }
+}
+
+pub fn add_response(
+    conn: &mut PgConnection,
+    _new_response: &IResponses,
+) -> Result<Responses, Box<dyn std::error::Error>> {
+    // inserting the new user
+    match diesel::insert_into(responses::table)
+        .values(_new_response)
+        .returning(Responses::as_returning())
+        .get_result(conn)
+    {
+        Ok(nr) => Ok(nr),
+        Err(e) => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("{:?}", e),
+            )))
+        }
+    }
 }
